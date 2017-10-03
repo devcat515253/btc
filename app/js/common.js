bd118ec2908330d4075db4037f2be970c4ff0800
alluans <alluans@yandex.ru>
2017-10-03 10:41:31 +0300

    add format number function

--- app/js/common.js
+++ app/js/common.js
@@ -103,10 +103,12 @@ $(function() {
     });
 
     $('.number input').bind("change keyup input click", function() {
-        if (this.value.match(/[^0-9.]/g)) {
-            this.value = this.value.replace(/[^0-9.]/g, '');
-        }
-        //this.value = this.value.replace(/(\d)(?=(\d{3})+([^\d.]|$))/g, '$1 ');
+         // if (this.value.match(/[^0-9]/g)) {
+         //    this.value = this.value.replace(/[^0-9.]/g, '');
+         // }
+       // this.value = this.value.replace(/(\d)(?=(\d{3})+([^\d.]|$))/g, '$1 ');
+        //this.value = this.value.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+        this.value = number_format(this.value, 2, '.', ' ');
     });
 
     $('.another-option >a').click(function () {
@@ -295,4 +297,31 @@ $(function() {
 
     activeDown();
 
+
+    function number_format(number, decimals, dec_point, thousands_sep) {
+        number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+        var n = !isFinite(+number) ? 0 : +number,
+            prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+            sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+            dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+            s = '',
+            toFixedFix = function(n, prec) {
+                var k = Math.pow(10, prec);
+                return '' + (Math.round(n * k) / k)
+                    .toFixed(prec);
+            };
+        // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+        s = (prec ? toFixedFix(n, prec) : '' + Math.round(n))
+            .split('.');
+        if (s[0].length > 3) {
+            s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+        }
+        if ((s[1] || '')
+                .length < prec) {
+            s[1] = s[1] || '';
+            s[1] += new Array(prec - s[1].length + 1)
+                .join('0');
+        }
+        return s.join(dec);
+    }
 });
