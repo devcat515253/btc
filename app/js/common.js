33f0913daeaf35d85fc83ccb274a581cf7368efd
alluans <alluans@yandex.ru>
2017-10-04 13:24:48 +0300

    fix

--- app/js/common.js
+++ app/js/common.js
@@ -332,67 +332,93 @@ $(function() {
    // var Highcharts = require('highcharts');
    // import {Highcharts} from '/highcharts'
 // Create the chart
-    Highcharts.chart('container', { /*Highcharts options*/ });
+    try {
+        Highcharts.chart('container', { /*Highcharts options*/ });
 
 
-    Highcharts.chart('container', {
-        chart: {
-            type: 'area',
-            spacingBottom: 30
-        },
-        title: {
-            text: 'Fruit consumption *'
-        },
-        subtitle: {
-            text: '* Jane\'s banana consumption is unknown',
-            floating: true,
-            align: 'right',
-            verticalAlign: 'bottom',
-            y: 15
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'left',
-            verticalAlign: 'top',
-            x: 150,
-            y: 100,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-        },
-        xAxis: {
-            categories: ['Apples', 'Pears', 'Oranges', 'Bananas', 'Grapes', 'Plums', 'Strawberries', 'Raspberries']
-        },
-        yAxis: {
-            title: {
-                text: 'Y-Axis'
+        Highcharts.chart('container', {
+            chart: {
+                type: 'area',
+                spacingBottom: 30
             },
-            labels: {
-                formatter: function () {
-                    return this.value;
+            title: {
+                text: 'Fruit consumption *'
+            },
+            subtitle: {
+                text: '* Jane\'s banana consumption is unknown',
+                floating: true,
+                align: 'right',
+                verticalAlign: 'bottom',
+                y: 15
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'left',
+                verticalAlign: 'top',
+                x: 150,
+                y: 100,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+            },
+            xAxis: {
+                categories: ['Apples', 'Pears', 'Oranges', 'Bananas', 'Grapes', 'Plums', 'Strawberries', 'Raspberries']
+            },
+            yAxis: {
+                title: {
+                    text: 'Y-Axis'
+                },
+                labels: {
+                    formatter: function () {
+                        return this.value;
+                    }
                 }
-            }
-        },
-        tooltip: {
-            formatter: function () {
-                return '<b>' + this.series.name + '</b><br/>' +
-                    this.x + ': ' + this.y;
-            }
-        },
-        plotOptions: {
-            area: {
-                fillOpacity: 0.5
-            }
-        },
-        credits: {
-            enabled: false
-        },
-        series: [{
-            name: 'John',
-            data: [0, 1, 4, 4, 5, 2, 3, 7]
-        }, {
-            name: 'Jane',
-            data: [1, 0, 3, null, 3, 1, 2, 1]
-        }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        this.x + ': ' + this.y;
+                }
+            },
+            plotOptions: {
+                area: {
+                    fillOpacity: 0.5
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'John',
+                data: [0, 1, 4, 4, 5, 2, 3, 7]
+            }, {
+                name: 'Jane',
+                data: [1, 0, 3, null, 3, 1, 2, 1]
+            }]
+        });
+    } catch(e){}
+
+
+
+
+    $('.twice  .step_content .drop-parent_responsive >a').on('click', function(event) {
+        event.preventDefault();
+        if($(this).next('.drop-content_responsive').is(":animated")) return;
+        if(window.matchMedia('(min-width: 540px)').matches){
+            return;
+        }
+
+        $(this).next('.drop-content_responsive').slideToggle();
+        $(this.parentNode).toggleClass('closed');
+        //console.log('wcw');
     });
+
+    // function closedDrop() {
+    //
+    //
+    //     //$('.twice .step_content .drop-parent_responsive.closed .drop-content_responsive').slideDown();
+    //     $('.twice .step_content .drop-parent_responsive.closed >a').next('.drop-content_responsive').slideDown();
+    //
+    // }
+    // closedDrop();
 });
